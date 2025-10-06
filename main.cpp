3888b5aa28f5e7dca42e10cbf3dbe1897b95023b
HarutoTanaka-go <k024g0048@g.neec.ac.jp>
2025-10-06 14:02:49 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <Windows.h>
+
+int main() {
+    //SetConsoleOutputCP(65001);
+    char str[] = "‚â‚Ù‚¤¡“ú‚Í“ú·‚µ‚ª‹­‚»‚¤‚â‚Ë";
+    printf("%s", str);
+    return 0;
+}
